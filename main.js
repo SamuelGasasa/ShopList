86d365fde6643c58a99a2b52ea43c4c0ec9eb8c3
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-24 20:22:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let input=document.querySelector('#item');
 function buttonClick(){
 const value=input.value;
 input.value='';
-let newLi=document.createElement('li');
-let newSpan=document.createElement('span');
-let newButton=document.createElement('button');
+const newLi=document.createElement('li');
+const newSpan=document.createElement('span');
+const newButton=document.createElement('button');
 newLi.appendChild(newSpan);
 newLi.appendChild(newButton);
 newSpan.textContent=value;
